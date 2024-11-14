2a299bccc9be64b8edb069f351ed2b2b80e4ff5b
Cedric <alecucedric@gmail.com>
2024-11-14 22:35:43 +0200

    added DatabaseHelper class to manage the conection and SQL queries

---
+++ Proiect3/DatabaseHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect3
+{
+    internal class DatabaseHelper
+    {
+        private string connectionString = "Server=G713RS;Database=spitaldb;Trusted_Connection=True;";
+
+        public SqlConnection GetConnection() { 
+            return new SqlConnection(connectionString);
+        }
+    }
+}
